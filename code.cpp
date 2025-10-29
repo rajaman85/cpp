bb7eb81e67f4727a1be771ac958f9d47c3648dcc
Aman <rajaman8594@gmail.com>
2025-10-29 14:00:32 +0530

    gvfdycGH

--- code.cpp
+++ code.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-struct node{
-    int data ;
-    node* next;
-};
+int main() {
+    int n;
+    cin >> n;  // size of the array
 
+    vector<char> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int m; // position (0-based)
+    cin >> m;
+
+    char newChar;
+    cin >> newChar;
+
+    // Insert the new character at position m
+    arr.insert(arr.begin() + m, newChar);
+
+    // Display updated array
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i];
+        if (i != arr.size() - 1)
+            cout << " ";
+    }
+
+    return 0;
+}
